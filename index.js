bcf5894962b161cc0340e8317a3d0c937c68450e
Max Morgenbesser <maxwellmorgenbesser@gmail.com>
2022-07-14 11:28:18 -0400

    arrays

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("Hello World")
+let fruits =['apple','banana','pear']
+fruits.push('watermelon')
+console.log(...fruits)
+fruits.unshift('mango')
+console.log(...fruits)
+fruits.pop()
+console.log("boats"+' '+fruits)
+console.log(fruits.indexOf('mango'))
+fruits.sort()
+console.log(fruits)
+
+const chickenEgg=["🐔","🥚"]
+comesFirst=chickenEgg.sort()
+console.log(comesFirst)
